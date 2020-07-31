f6f6fb41e273b2770332a9c7a05cc331ae61937b
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 21:19:41 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,2 +1,12 @@
 require 'pry'
 
+def my_all?(container)
+  empty_container= []
+  counter= 0 
+  
+  while counter < counter.length 
+  empty_container << yield(container[counter])
+  counter += 1 
+end 
+
+end 
