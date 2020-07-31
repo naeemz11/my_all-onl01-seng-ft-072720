a593e1ba286bc415717c0a28f5186b45c5042c5d
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 21:32:07 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
-def my_all?(collection)
+def my_all?(container)
+  empty_container= []
+  counter= 0 
+  
+  while counter < container.length 
+  empty_container << yield(container[counter])
+  counter += 1 
+end 
+  
+   if empty_container.include?(false)
+     false
+   else 
+     true 
+   end 
+end 
 
-end
